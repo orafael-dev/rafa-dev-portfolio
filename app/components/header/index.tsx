5c4343d399d0431516ced53dd3b78de05e0fc5c5
Rafael Almeida <almeidarp.dev@gmail.com>
2024-10-28 22:14:16 -0300

    feat: Create navbar component and add styles

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,10 +1,40 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import {NavItem} from "./nav-items";
+
+const NAV_ITEMS = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Projetos",
+    href: "/projects",
+  },
+];
+
 export const Header = () => {
   return (
-    <header>
-      <div className="container">
-        <Link href="/">
-          <Image src="/logo.svg" alt="logo" width={58} height={49} />
+    <header className="absolute top-0 w-full z-10 h-24 flex items-center justify-center">
+      <div className="container flex items-center justify-between">
+        <Link
+          href="/"
+          className="text-gray-400 flex items-center gap-2 font-medium font-mono"
+        >
+          <Image
+            width={42}
+            height={40}
+            src="/images/logo.svg"
+            alt="Logo Rafael"
+          />
         </Link>
+        <nav className="flex items-center gap-4 sm:gap-10 ">
+          {NAV_ITEMS.map((item) => (
+            <NavItem key={item.label} {...item} />
+          ))}
+        </nav>
       </div>
     </header>
   );
