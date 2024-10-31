565faca105e59d5c4443901d56f621842406dfc8
Rafael Almeida <almeidarp.dev@gmail.com>
2024-10-30 23:11:10 -0300

    feat: implements Highlight section and add provisory wireframe image

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import {HeroSection} from "./components/pages/home/hero-section";
 import {KnownTechs} from "./components/pages/home/knowns-techs";
+import {HighlightProjects} from "./components/pages/home/highligted-projects/highlight-projects";
 
 export default async function Home() {
   return (
     <>
       <HeroSection />
       <KnownTechs />
+      <HighlightProjects />
     </>
   );
 }
