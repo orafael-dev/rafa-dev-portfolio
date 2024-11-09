45e341982425ac526e87ae5beb85da1083c1ca8f
Rafael Almeida <almeidarp.dev@gmail.com>
2024-11-09 11:03:57 -0300

    fix: fixes the link component to use clsx

--- app/components/link/index.tsx
+++ app/components/link/index.tsx
@@ -7,12 +7,10 @@ type LinkProps = ComponentProps<typeof NextLink>;
 export const Link = ({className, children, ...props}: LinkProps) => {
   return (
     <NextLink
-      className={
-        (cn(
-          "flex items-center gap-2 text-gray-500 text-sm hover:text-emerald-500 transition-colors"
-        ),
-        className)
-      }
+      className={cn(
+        "flex items-center gap-2 text-gray-300 text-sm hover:text-emerald-500 transition-colors",
+        className
+      )}
       {...props}
     >
       {children}
